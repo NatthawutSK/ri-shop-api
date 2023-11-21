e6b4b89ea54a0040c959f9fb5eefb1f26b835f1b
FixFire <89625074+NatthawutSK@users.noreply.github.com>
2023-11-21 10:11:15 +0700

    done signout

--- modules/users/usersUsecases/usersUsecase.go
+++ modules/users/usersUsecases/usersUsecase.go
@@ -14,6 +14,7 @@ type IUserUsecase interface{
 	InsertCustomer(req *users.UserRegisterReq) (*users.UserPassport, error)
 	GetPassport(req *users.UserCredential) (*users.UserPassport, error)
 	RefreshPassport(req *users.UserRefreshCredential) (*users.UserPassport, error)
+	DeleteOauth(oauthId string) error
 }
 
 type UserUsecase struct {
@@ -135,5 +136,13 @@ func (u *UserUsecase) RefreshPassport(req *users.UserRefreshCredential) (*users.
 	}
 
 	return passport, nil
+	
+}
+
+func (u *UserUsecase) DeleteOauth(oauthId string) error {
+	if err := u.usersRepository.DeleteOauth(oauthId); err != nil {
+		return err
+	}
+	return nil
 	
 }
